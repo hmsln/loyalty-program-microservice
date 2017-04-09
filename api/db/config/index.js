b1561c8c420e2bcf602c1871f4a1851b4c46d75e
Hubert <hubert.maslin@gmail.com>
2017-04-09 18:27:44 +0200

    removed isOnAWS check

--- api/db/config/index.js
+++ api/db/config/index.js
@@ -1,5 +1,5 @@
 var config = {
-    "mongoUrl": process.env.IS_ON_AWS == undefined ? 'mongodb://app2:whatpeoplesaw@ds147900.mlab.com:47900/enriched-chat' : process.env.MONGO_URL
+    "mongoUrl":'mongodb://app2:whatpeoplesaw@ds147900.mlab.com:47900/enriched-chat'
 }
 
 module.exports = config;
